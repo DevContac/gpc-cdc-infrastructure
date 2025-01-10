a652a0ef352ab8f880617a80fe551098016e3ef4
jgonzalez-m <jorgej.gonzalezmartinez@gmail.com>
2025-01-10 12:56:50 -0300

    feat: data pb

---
+++ pocketbase/pb_migrations/1736454513_updated_area_code.js
@@ -0,0 +1,18 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("tee1z8x4b24seq1")
+
+  collection.listRule = ""
+  collection.createRule = null
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("tee1z8x4b24seq1")
+
+  collection.listRule = null
+  collection.createRule = ""
+
+  return dao.saveCollection(collection)
+})
